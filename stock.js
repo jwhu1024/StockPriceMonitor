cc456fc5000fdbedcb039dd412999f3926966eb5
jwhu1024 <jwhu1024@gmail.com>
2015-01-21 11:55:21 +0800

    - print the result in the one line when the price has changed. - fix syntax error

--- stock.js
+++ stock.js
@@ -1,38 +1,49 @@
 // http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_3474.tw_20140801&json=1&delay=0
-var http = require('http'),
-    dateFormat = require('dateformat');
+var http 		= require('http'),
+    dateFormat	= require('dateformat');
 
-STOCK_PREFIX	= "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_"
-STOCK_CODE		= "2455";
+STOCK_PREFIX	= "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_";
+STOCK_CODE		= "1234";
 STOCK_INFIX 	= ".tw_";
 STOCK_DATE		= dateFormat(new Date(), "yyyymmdd");
-STOCK_SUFFIX 	= "&json=1&delay=0"
+STOCK_SUFFIX 	= "&json=1&delay=0";
 
-STOCK_THRESHOLD = 0;
-QUERY_TIMEOUT	= 5000;
+STOCK_THRESHOLD = 10;
+QUERY_TIMEOUT	= 10000;
 
 strUrl = STOCK_PREFIX + STOCK_CODE + STOCK_INFIX + STOCK_DATE + STOCK_SUFFIX;
 
 console.log('URL we ready to Get : \n' + strUrl);
+console.log('--------------------------------------\n');
 
-var data = '';
+var old_price = '';
+
 setInterval(function() {
 	http.get(strUrl, function(res) {
+		var data = '';
 		res.on("data", function(chunk) {
 			data += chunk;
 		});
-		res.on("end", function(){
+		res.on("end", function() {
 			var timeStamp = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
 			var string = JSON.parse(data);
 			
-			console.log('[' + timeStamp + ']' + 
-						' Stock Code\t:' + string.msgArray[0].c + '  |  ' +
-						'Stock Price\t:' + string.msgArray[0].z);
+			if (old_price !== string.msgArray[0].z) {
+				process.stdout.clearLine();  // clear current text
+				process.stdout.cursorTo(0);  // move cursor to beginning of line
+				process.stdout.write('[' + timeStamp + ']' + 
+									 ' Code:' + string.msgArray[0].c + '  |  ' +
+									 'Price:' + string.msgArray[0].z);
+									 
+				// Save the older price
+				old_price = string.msgArray[0].z;
+			} else {
+				process.stdout.write('.');
+			}
 
-			if (STOCK_THRESHOLD > 0 && STOCK_THRESHOLD >= string.msgArray[0].z) {
+			if (STOCK_THRESHOLD > 0 &&  string.msgArray[0].z >= STOCK_THRESHOLD) {
 				console.log('Threshold Arrived!!!');
 			}
-			data = '';
 		});
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
