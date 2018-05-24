bec99d810785255a1bcb527c3a4070950b47b91a
vagrant <vagrant@python3>
2018-05-24 09:37:03 +0000

    1. fixed http request error, because the website has been changed the format of http GET.

--- stock.js
+++ stock.js
@@ -1,61 +1,63 @@
-// http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_1234.tw&json=1&delay=0&_=1527140228574
-var http 		= require('http'),
-    dateFormat	= require('dateformat');
+// http://mis.twse.com.tw/stock/fibest.jsp?stock=2317
+// http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_2317.tw&json=1&delay=0&_=1527146997659
 
-STOCK_PREFIX	= "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_";
-STOCK_CODE		= "1234";
-STOCK_INFIX 	= ".tw&json=1&delay=0&_=";
-STOCK_TIMESTAMP	= new Date().getTime();
+var request = require('request'),
+	dateFormat	= require('dateformat');
+	
+var STOCK_THRESHOLD = 10,
+	QUERY_TIMEOUT	= 1000,
+	URL				= "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_",
+	STOCK_ID 		= "2317",
+	URL_INF			= ".tw&json=1&delay=0&_=";
 
-STOCK_THRESHOLD = 10;
-QUERY_TIMEOUT	= 1000;
+function getCookies (callback) {
+    request("http://mis.twse.com.tw/stock/fibest.jsp?stock=" + STOCK_ID, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            return callback(null, response.headers['set-cookie']);
+        } else {
+            return callback(error);
+        }
+    })
+}
 
-strUrl = STOCK_PREFIX + STOCK_CODE + STOCK_INFIX + STOCK_TIMESTAMP;
+function getStockInfo (cookie, callback) {
+	var options = {
+		url: URL + STOCK_ID + URL_INF + new Date().getTime(),
+		headers: {'Cookie': cookie}
+	};	
 
-console.log('URL we ready to Get : \n' + strUrl);
-console.log('--------------------------------------\n');
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			callback(JSON.parse(body));
+		}
+	});
+}
 
-var old_price = '';
+function printInOneLine (string) {
+	var timeStamp = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss")
+	process.stdout.clearLine();  // clear current text
+	process.stdout.cursorTo(0);  // move cursor to beginning of line
+	process.stdout.write('[' + timeStamp + '] ' + string);
+}
 
-setInterval(function() {
-    var retry_int = setInterval(function() {
-	http.get(strUrl, function(res) {
-    		var data = '';
-    		res.on("data", function(chunk) {
-    			data += chunk;
-    		});
-    		res.on("end", function() {
-    			var timeStamp = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
-    			var string = JSON.parse(data);
-    			
-                if (string.msgArray === undefined) {
-                    console.log("undefined msgArray");
-                    clearInterval(retry_int);
-                } else {
-
-    			    if (old_price !== string.msgArray[0].z) {
-    			    	process.stdout.clearLine();  // clear current text
-    			    	process.stdout.cursorTo(0);  // move cursor to beginning of line
-    			    	process.stdout.write('[' + timeStamp + ']' + 
-    			    						 ' Code:' + string.msgArray[0].c + '  |  ' +
-    			    						 'Price:' + string.msgArray[0].z);
-    			    						 
-    			    	// Save the older price
-    			    	old_price = string.msgArray[0].z;
-    			    } else {
-    			    	process.stdout.write('.');
-    			    }
-     
-     			    if (STOCK_THRESHOLD > 0 &&  string.msgArray[0].z >= STOCK_THRESHOLD) {
-    				    console.log('Threshold Arrived!!!');
-    			    }
-                }
-    		});
-    	}).on('error', function(e) {
-		    console.log("Got error: " + e.message);
-	    });
-    }, QUERY_TIMEOUT);
-}, QUERY_TIMEOUT);
-
-// process.stdout.clearLine();  // clear current text
-// process.stdout.cursorTo(0);  // move cursor to beginning of line
+// entry
+getCookies(function (err, cookie){
+	if(!err) {
+		// console.log('Get the cookie --> ' + cookie);
+		
+		setInterval(function () {
+			getStockInfo(cookie, function (res) {
+				var fullData = res;
+				
+				if ( typeof fullData.msgArray !== 'undefined' && fullData.msgArray ) {
+					printInOneLine (' Code:' + fullData.msgArray[0].c + '  |  ' +
+									'Price:' + fullData.msgArray[0].z)
+				} else {
+					printInOneLine ("waiting for retry in " + QUERY_TIMEOUT/1000 + " seconds...")
+				}
+			});
+		}, QUERY_TIMEOUT);
+	} else {
+		console.log("get cookie error");
+	}
+})
